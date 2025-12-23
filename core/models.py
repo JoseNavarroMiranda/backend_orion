5db5d81463fe9e267fa94c2cc9fac095c36a0a09
JoseNavarroMiranda <nqvarr01234@gmail.com>
2025-12-22 19:55:07 -0600

    models add in project

--- core/models.py
+++ core/models.py
@@ -1,1 +1,21 @@
+import uuid
 from django.db import models
+
+class Collaborator(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+    role = models.CharField(max_length=200)
+    description = models.CharField(max_length=250, null=True, blank=True)
+#    image_field = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class StackInfo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    description_service = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
