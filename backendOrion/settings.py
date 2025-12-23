4b637379c0644617cbe1407cb46d5a6c11692663
JoseNavarroMiranda <nqvarr01234@gmail.com>
2025-12-22 20:19:42 -0600

    add of model and interaction with bd

--- backendOrion/settings.py
+++ backendOrion/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,9 +74,16 @@ WSGI_APPLICATION = 'backendOrion.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE", "orion_DB"),
+        "USER": os.getenv("MYSQL_USER", "orion"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD", "orionpass"),
+        "HOST": os.getenv("MYSQL_HOST", "db"),
+        "PORT": os.getenv("MYSQL_PORT", "3306"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
