a8c9df59679720782641dc23ff34c93e1b6460a6
JoseNavarroMiranda <nqvarr01234@gmail.com>
2026-01-12 18:15:48 -0600

    adding endpoint for react

--- evaluations/urls.py
+++ evaluations/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path # type: ignore
-from .views import StackInfoCreateAPIView
+from .views import StackInfoCreateAPIView, CollaboratorListAPIView
 
 app_name = "evaluations"
 
 urlpatterns = [
-    path("stackinfo/", StackInfoCreateAPIView.as_view(), name = "stackinfo")
+    path("stackinfo/", StackInfoCreateAPIView.as_view(), name = "stackinfo"),
+    path("collaborator/", CollaboratorListAPIView.as_view(), name = "collaborator"),
 ]
